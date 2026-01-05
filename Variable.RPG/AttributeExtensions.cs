f79415df3626282797a51195faa455ffd2f4f1bc
vex.iafahim <iafahim.dev@gmail.com>
2026-01-05 07:20:22 -0500

    Added Missing XML AttributeExtensions.cs

--- Variable.RPG/AttributeExtensions.cs
+++ Variable.RPG/AttributeExtensions.cs
@@ -6,6 +6,10 @@ namespace Variable.RPG;
 /// </summary>
 public static class AttributeExtensions
 {
+    /// <summary>
+    ///     Recalculates the cached value of the attribute based on base value and modifiers.
+    /// </summary>
+    /// <param name="attr">The attribute to recalculate.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void Recalculate(ref this Attribute attr)
     {
@@ -20,6 +24,12 @@ public static class AttributeExtensions
         );
     }
 
+    /// <summary>
+    ///     Adds a modifier to the attribute.
+    /// </summary>
+    /// <param name="attr">The attribute to modify.</param>
+    /// <param name="flat">The flat amount to add.</param>
+    /// <param name="percent">The percentage to add (1.0 = 100%).</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void AddModifier(ref this Attribute attr, float flat, float percent)
     {
@@ -32,6 +42,10 @@ public static class AttributeExtensions
         );
     }
 
+    /// <summary>
+    ///     Clears all modifiers from the attribute.
+    /// </summary>
+    /// <param name="attr">The attribute to clear modifiers from.</param>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static void ClearModifiers(ref this Attribute attr)
     {
@@ -42,6 +56,11 @@ public static class AttributeExtensions
         );
     }
 
+    /// <summary>
+    ///     Gets the current calculated value of the attribute.
+    /// </summary>
+    /// <param name="attr">The attribute to get the value from.</param>
+    /// <returns>The calculated value.</returns>
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static float GetValue(ref this Attribute attr)
     {
